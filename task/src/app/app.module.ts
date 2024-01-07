364c279e54b314431848a5c922cc76f862bc6246
Leonid <leonideko1@gmail.com>
2024-01-07 18:47:00 +0200

    write expression

--- task/src/app/app.module.ts
+++ task/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-
-
 
 @NgModule({
   declarations: [AppComponent, FormComponent],
--- task/src/app/form/form.component.ts
+++ task/src/app/form/form.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  password: string = '';
+  strength: string[] = ['', '', ''];
 
+  updateStrength() {
+    const length = this.password.length;
+
+    if (length === 0) {
+      this.strength = ['', '', ''];
+      this.strength = ['gray', 'gray', 'gray'];
+    } else if (length < 8) {
+      this.strength = ['red', 'red', 'red'];
+    } else {
+      const hasLetters = /[a-zA-Z]/.test(this.password);
+      const hasDigits = /\d/.test(this.password);
+      const hasSymbols = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(this.password);
+
+      if (hasLetters && hasDigits && hasSymbols) {
+        this.strength = ['green', 'green', 'green'];
+      } else if (
+        (hasLetters && hasDigits) ||
+        (hasLetters && hasSymbols) ||
+        (hasDigits && hasSymbols)
+      ) {
+        this.strength = ['yellow', 'yellow', 'gray'];
+      } else if (hasLetters || hasDigits || hasSymbols) {
+        this.strength = ['red', 'gray', 'gray'];
+      } else {
+        this.strength = ['gray', 'gray', 'gray'];
+      }
+    }
+  }
 }
